702e99978883e7ba9a30b0fdd67553dfc1ea62ba
SinkTried <justin.centeno@lc.cuny.edu>
2025-09-19 01:31:29 -0400

    Add files via upload

---
+++ VrArcadeScript.sql
@@ -0,0 +1,121 @@
+- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema vr_arcade
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema vr_arcade
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `vr_arcade` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `vr_arcade` ;
+
+-- -----------------------------------------------------
+-- Table `vr_arcade`.`customer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vr_arcade`.`customer` (
+  `CustomerID` INT NOT NULL AUTO_INCREMENT,
+  `CustomerName` VARCHAR(100) NOT NULL,
+  `CustomerEmail` VARCHAR(100) NOT NULL,
+  `CustomerNumber` INT NOT NULL,
+  PRIMARY KEY (`CustomerID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `vr_arcade`.`employees`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vr_arcade`.`employees` (
+  `EmpId` INT NOT NULL AUTO_INCREMENT,
+  `EmpFirstName` VARCHAR(45) NOT NULL,
+  `EmpLastName` VARCHAR(45) NOT NULL,
+  `DOB` VARCHAR(45) NOT NULL,
+  `ContactNumber` INT NOT NULL,
+  `Email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`EmpId`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `vr_arcade`.`games`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vr_arcade`.`games` (
+  `GamesId` INT NOT NULL AUTO_INCREMENT,
+  `GameName` VARCHAR(50) NOT NULL,
+  `GameStatus` VARCHAR(1) NULL DEFAULT 'R' COMMENT 'Check if the game is in working condition. R for running D for down',
+  `Package` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`GamesId`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `vr_arcade`.`package`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vr_arcade`.`package` (
+  `packID` INT NOT NULL AUTO_INCREMENT,
+  `PackageName` VARCHAR(100) NOT NULL,
+  `PackagePrice` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`packID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `vr_arcade`.`reservation`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vr_arcade`.`reservation` (
+  `ResId` INT NOT NULL AUTO_INCREMENT,
+  `Customer Name` VARCHAR(100) NOT NULL,
+  `ResDate` VARCHAR(45) NOT NULL,
+  `Package` VARCHAR(45) NOT NULL,
+  `Payment` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ResId`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+use vr_arcade;
+
+Insert into customer (CustomerName,CustomerEmail,CustomerNumber)
+values ('John Smith','john.smith@email.com','1234567891');
+
+Insert into employees (EmpFirstName,EmpLastName,DOB,ContactNumber,Email)
+values ('Bruce','Wayne','4-7-1915','1234567891','b.wayne@email.com');
+
+Insert into games (GameName,GameStatus,Package)
+values ('Asphalt 9 Legends Aracede','R','Regular Pacakage');
+
+Insert into reservation (CustomerName,ResDate,Package)
+values ('John Smith','01-01-2026','Regular Pacakage');
+
+Select * from customer;
+
+Select * from employees;
+
+Select * from games;
+
+SELECT 
+    *
+FROM
+    reservation;
